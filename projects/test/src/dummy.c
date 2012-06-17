7fa07069f20d208838986bee061f35ae0cfba91b
matma6 <matma6@matma6.net>
2012-06-17 13:49:27 +0200

    Added second file with C source

---
+++ projects/test/src/dummy.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void dummy()
+{
+	printf("Hello, this is a dummy\n");
+}
--- projects/test/src/test.c
+++ projects/test/src/test.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+#include "dummy.h"
+
 int main()
 {
 	printf("Hello. This is ATmegOS!\n");
+	dummy();
+
 	return 0;
 }
 
